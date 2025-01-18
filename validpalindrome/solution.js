fb92495227cb62a80336bf075cbba912c3b813d0
anisah majeed <anisahmajeed4@gmail.com>
2025-01-18 16:39:15 -0600

    valid palindrome

---
+++ validpalindrome/solution.js
@@ -0,0 +1,12 @@
+const isPalindrome = function(s) {
+    const string = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    const reversedString = string.split("").reverse().join("");
+
+    if (string === reversedString) {
+        return true;
+    }
+
+    return false;
+};
+
+//leetcode accepted
